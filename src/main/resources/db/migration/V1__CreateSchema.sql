8ea68098a68e637796a69788969eec042f451772
KristofferHaakonsen <kristofferhaakonsen@gmail.com>
2023-11-02 18:41:54 +0100

    wip snart

---
+++ src/main/resources/db/migration/V1__CreateSchema.sql
@@ -0,0 +1,4 @@
+create scema if not exists cybriabank;
+
+grant usage on schema cybriabank to "${DB_USERNAME}";
+
---
+++ src/main/resources/db/migration/V2__CreateTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS cybriabank.customer (
+    name VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    social_security_number VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    postal_code VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    PRIMARY KEY (social_security_number)
+);
+
+grant SELECT, INSERT on customer to "${dbUsername}";
+
